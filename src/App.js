9a83c0c4dbea0e58587b1b4e9082ec67f73cb1c5
unknown <devindishakya1@gmail.com>
2025-03-16 16:59:54 +0530

    Made some changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={xoxo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          devindi shakya
+          CLICK HERE TO GO TO THE REACT WEBSITE BY DEVINDI
         </a>
       </header>
     </div>
